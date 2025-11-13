166c54c6fedb354d209f11a742e62eaa54c30cc7
hasanzaidi-lab <hasan.zaidi@techconsulting.tech>
2025-11-13 14:48:02 -0500

    improved docs and refactored for MVVM-C model

--- AvApp/AvApp/Features/MapKitInteg/Views/MapKitView.swift
+++ AvApp/AvApp/Features/MapKitInteg/Views/MapKitView.swift
@@ -9,17 +9,18 @@ import SwiftUI
 import MapKit
 
 public struct MapKitView: View {
-    @StateObject private var viewModel: MapKitViewModel
-    private let icao24: String
+    @StateObject private var coordinator: MapKitCoordinator
+    @ObservedObject private var viewModel: MapKitViewModel
     
     public init(icao24: String) {
-        self.icao24 = icao24
-        _viewModel = StateObject(wrappedValue: MapKitViewModel())
+        let coordinator = MapKitCoordinator(icao24: icao24)
+        _coordinator = StateObject(wrappedValue: coordinator)
+        _viewModel = ObservedObject(wrappedValue: coordinator.viewModel)
     }
 
-    init(icao24: String, viewModel: MapKitViewModel) {
-        self.icao24 = icao24
-        _viewModel = StateObject(wrappedValue: viewModel)
+    init(coordinator: MapKitCoordinator) {
+        _coordinator = StateObject(wrappedValue: coordinator)
+        _viewModel = ObservedObject(wrappedValue: coordinator.viewModel)
     }
     
     public var body: some View {
@@ -61,8 +62,11 @@ public struct MapKitView: View {
                     .foregroundColor(.secondary)
             }
         }
-        .task(id: icao24) {
-            viewModel.loadTrack(for: icao24)
+        .task(id: coordinator.icao24) {
+            coordinator.loadTrack()
+        }
+        .onAppear {
+            coordinator.onAppear()
         }
     }
 }
